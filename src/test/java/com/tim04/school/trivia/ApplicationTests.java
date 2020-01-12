e080379e7c83f60eec17faa39679133659192c5f
eve-prog <strugurel123->
2020-01-12 15:00:47 +0200

    Initial commit

---
+++ src/test/java/com/tim04/school/trivia/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim04.school.trivia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
